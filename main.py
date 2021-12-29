f651db7c5383bdb49c57f011d8ba6bf7b32185bb
ytshih <ryanytshih@gmail.com>
2021-12-29 22:33:42 +0800

    write pwd csv file

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # -*- coding:utf-8 -*-
+import argparse
+import csv
 import os
 import random
-import argparse
+import re
 import string
 
 parser = argparse.ArgumentParser()
@@ -52,28 +54,69 @@ def create():
     base_image = 'udic_hadoop:latest'
     master_name_prefix = '%d_udic_hadoop_master_'%random_prefix
     slave_name_prefix = '%d_udic_hadoop_slave_'%random_prefix
+
+    host_info_dict = {}
+    fieldnames = ["index", "host_type", "account", "password", "ssh_port", "web_port"]
+    idx = 0
     for m_i in range(hadoop_should_create_count):
         pwd_log.write("------------%d------------\n"%(m_i))
+
         master_name = master_name_prefix + str(m_i)
         master_user_name = master_name
         master_user_pwd = get_random_string(6)
+
         pwd_log.write("%s\n"%master_user_name)
         pwd_log.write("%s\n"%master_user_pwd)
         pwd_log.write("\n")
+
+        host_info_dict[master_user_name] = {
+            "index": idx,
+            "host_type": "master",
+            "account": master_user_name,
+            "password": master_user_pwd
+        }
+        idx += 1
 
         cmd('docker run --name %s --hostname %s -itd -p 22 -p 8088 -p 8080 -p 8000 -p 8888 -e"NAME"=%s -e"PASSWORD"=%s %s'%(master_name,master_name,master_user_name,master_user_pwd,base_image))
         for s_i in range(slave_should_create_count):
             slave_name = slave_name_prefix + '%d-%d'%(m_i,s_i)
             slave_user_name = slave_name
             slave_user_pwd = get_random_string(6)
+
             pwd_log.write("%s\n"%slave_user_name)
             pwd_log.write("%s\n"%slave_user_pwd)
             pwd_log.write("\n")
+
+            host_info_dict[slave_user_name] = {
+                "index": idx,
+                "host_type": "slave",
+                "account": slave_user_name,
+                "password": slave_user_pwd
+            }
+            idx += 1
             cmd('docker run --name %s --hostname %s -itd -p 22 -e"NAME"=%s -e"PASSWORD"=%s %s'%(slave_name,slave_name,slave_user_name,slave_user_pwd,base_image))
     
     # show create
-    create_result = os.popen('docker ps --format "{{.Names}}: {{.Ports}}" -f "name=%d_udic_hadoop_"'%random_prefix).read().split("\n")
+    create_result = os.popen('docker ps --format "{{.Names}}, {{.Ports}}" -f "name=%d_udic_hadoop_"'%random_prefix).read().split("\n")
     create_result.pop(-1)
+
+    # get ports
+    for r in create_result:
+        username = r.split(",")[0]
+        match = re.search(r"0.0.0.0:(\d+)->22/tcp", r)
+        host_info_dict[username]["ssh_port"] = match.group(1)
+        match = re.search(r"0.0.0.0:(\d+)->8088/tcp", r)
+        if match:
+            host_info_dict[username]["web_port"] = match.group(1)
+    
+    # write csv file
+    with open("pwd.csv", "w", newline="") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames)
+        writer.writeheader()
+        host_info_rows = sorted(host_info_dict.values(), key=lambda d: d["index"])
+        for row in host_info_rows:
+            writer.writerow(row)
+        
 
     for i,r in enumerate(create_result):
         r = ' '.join(r.split())
